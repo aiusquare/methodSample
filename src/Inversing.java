253dd3060ea6caa373ccaceaa86c14a105edcc37
Default <Default@JamerPogi>
2020-03-17 10:00:45 +0800

    my Reversing

--- src/Inversing.java
+++ src/Inversing.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+
 
 class Inversing{
     
      public static void main(String args[]){
-     System.out.println("Enter a String");
+     System.out.print("Enter a String  :");
         Inversing never = new Inversing();
         never.input();
-        never.saying();
+        Inversing.saying();
         never.Reverse();
          }
-     static  String []input=new String [1];
-     public  String input(){
+     
+     static  String input[] = new String[1];
+     public  String[] input(){
        
          Scanner Input = new Scanner(System.in);
         input[0] = Input.nextLine();
-         return input[0];
+         return input;                 
      }
+    
      public void Reverse(){
-         String result="";
-         for(int i=input.length-1;i>=0;i--){
-             result= result+(input[i]);
-            System.out.println(result);
+         String type []= new String [input.length];
+         int j = input.length;
+
+         for(int i=0;i>j-i-1;i--){
+          if (ij){
+            System.out.println(input[i]);
          }
-         
      }
-     public void saying(){ 
+     }
+          public static void saying(){
          System.out.println("The entered String is :"+input[0]);
 
      }
@@ -36,4 +38,4 @@ class Inversing{
          
      
 
-    
+     
