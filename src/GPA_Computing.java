3df10b4ece919ff281ef807eb901578417e9c69e
aiusquare <ahmadibrahimusmanmkk@gmail.com>
2020-03-20 01:46:41 +0100

    my changes

--- src/GPA_Computing.java
+++ src/GPA_Computing.java
@@ -1,164 +1,89 @@
 import java.util.Scanner;
 import java.util.*;
 
-
-
  class GPA_Computing {
-
-  
-    public void Front(){
+    
+    public void front(){
         System.out.println("GPA Computing System");
         System.out.println("The Program Starts!");
         System.out.println("");
         System.out.println("");
+  
         /*
         Process is starting
         */
-        String Score = "";
-        int unit=0;
-        int Power=0;
-       
-        GPA_Computing.S(Score);
-        
-       GPA_Computing.unit(unit);
-
-       
-        
-        GPA_Computing.tally();
-         
+        compute(score(),getCreaditUnit()); 
     }
-    
-    private static final String Score[] = new String [7];
 
-    public static String S(String score){
-       
-        
-        Scanner Students =new Scanner (System.in);
-       
+    public int[] score(){
+        Scanner scoreInput =new Scanner (System.in);
+        int[] scores= new int[7];
+        String[] grades = new String [7];
         /*
         Entering Score Field
         
         */
-        for(int Sc=0;Sc<Score.length;Sc++){
-        
-        System.out.print("Enter your Score :");
-        Score[Sc]=Students.nextLine();
-        
+        for(int i=0;i<grades.length;i++){
+            System.out.print("Enter your Score :");
+            grades[i] = scoreInput.nextLine();
         }
         /*
         Showing entered score in a list
         */
        
-        for(String  list :Score){
-            Arrays.asList(list);
-            System.out.println("\nList of Score: "+list);
-            equivalent(list); 
+        for(int i=0;i<scores.length;i++){
+            scores[i]=equivalent(grades[i]);
         }
         
-        return score;
+        return scores;
+    }
+ 
+    public int[] getCreaditUnit(){
+        Scanner creaditUInput =new Scanner(System.in);
+        int[] creaditUnits=new int[7];
+        
+        for(int i =0;i<creaditUnits.length;i++){
+           creaditUnits[i] = creaditUInput.nextInt();
+        }
+        return creaditUnits;
     }
     
-    static int Un;
+    public void compute(int[] score, int[] crdUnit){
+       double gpa=0;
+       for(int i=0;i<score.length;i++){
+           System.out.println("The score points: "+score[i]);
+       }
   
-    public static int unit(int unit){
-        /*
-       Entering Unit field
-        */
-        Scanner Student = new Scanner(System.in);
-        int UnitTotal []= new int [7];
-       
-        for(  Un =0;Un<7;Un++){
-            System.out.println("");
-            System.out.print("Enter how many Unit: ");
-            UnitTotal[Un]= Student.nextInt();
-        }
-     
-          /*
-      List of Unit entered Field
-        */
-          
-        int sum=0;
-        
-        for( int List :UnitTotal){
-            Arrays.asList(List);
-            sum=sum+=List;
-            System.out.println("The list of entered Unit is :"+List);
-        }Computing(sum);
-        
-      
-        
-        
-        return sum;
-    }
-    private static int total;
-    public static int Computing(int Power){
-        total = Power;
-       System.out.println("Sum of total is "+total);
-     
-       return Power;
+       for(int i=0;i<crdUnit.length;i++){
+           System.out.println("The credit units: "+crdUnit[i]);
+       }
     }
     
-    private static String scoreEq;
-    private static int uno,dos,tres,kwatro,betlog;
-    public static String equivalent(String equivalent){
-       scoreEq= equivalent;{
+    public int equivalent(String gradePoint){
+       int point=0;
         /*
        Equivalent of  Score
        */
-        switch(scoreEq){
+        switch(gradePoint){
            case "A":
-                System.out.printf("The Equivalent of %s Score is :",scoreEq);
-              kwatro=4;
-               System.out.print(kwatro);
-               
+               point=5;
                break;
            case "B":
-              
-                System.out.printf("The Equivalent of %s Score is :",scoreEq);
-                tres =3;
-                System.out.print(tres);
-           
+                point =4;
                 break;
            case "C":
-                System.out.printf("The Equivalent of %s Score is :",scoreEq);
-                dos=2;
-                System.out.print(dos);
+                point=3;
                 break;
            case "D":
-                System.out.printf("The Equivalent of %s Score is :",scoreEq);
-                uno =1;
-                System.out.print(uno);
+                point =2;
                 break;
            case"E":
-                System.out.printf("The Equivalent of %s Score is :",scoreEq);
-                betlog=0;
-                System.out.print(betlog);
+                point=1;
                 break;
            case "F":
-               System.out.print("Out of lsit");
+               point=0;
                break;
-           case "G":
-               System.out.print("Out of list");
-               break;
-       }TotalScore(uno,dos,tres,kwatro,betlog);
- 
-return equivalent;
-    }
-    }
-
-    private static int Tally;
-    public static int TotalScore(int TotalScore1,int TotalScore2,int TotalScore3,int TotalScore4,int TotalScore5){
-Tally = TotalScore1;
-  
-  /*
-Sir check this out
-*/
-         
- 
-  return Tally;
-}
-
-public static void tally (){
-     System.out.println("hello"+Tally);
-}
+          }
+          return point;
+      }
 }
