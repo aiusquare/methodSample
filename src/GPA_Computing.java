9fd7f32d2b23f5eb485a095206e403ac7bed77a7
Default <Default@JamerPogi>
2020-04-01 08:48:01 +0800

    April 1/2020 9:23 PM in Philippines

--- src/GPA_Computing.java
+++ src/GPA_Computing.java
@@ -54,7 +54,7 @@ import java.util.Scanner;
             System.out.print("Enter the credit unit :");
               
            creaditUnits[i] = creaditUInput.nextInt();
-        }   
+        }   //hello
         return creaditUnits;
     }
    
